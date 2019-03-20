7c78c200113ef1f09999c925eccbc509a01e144c
YagoGodoyGarcia <yagogodoygarcia@outlook.com>
2019-03-20 19:34:58 -0300

    alterações campos padões

--- src/main/java/br/com/dia/etiquetas/services/tfotmer/GeneratorPdf.java
+++ src/main/java/br/com/dia/etiquetas/services/tfotmer/GeneratorPdf.java
@@ -17,61 +17,9 @@ public class GeneratorPdf {
 
 	public GeneratorPdf(JSONArray json, String tipo) throws TFormerException {		  
 		if(json.length() != 0) {
-		if(tipo.equals("etiquetaUmQuadranteNormalOferta")) {
 			Etiquetar(json, tipo);
 		}
-//		}else if(tipotipo.equals("etiquetaUmQuadranteClubDia")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaUmQuadranteComboLevePague")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaUmQuadranteComboAPartir")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaUmQuadranteComboPorcUmaUnidade")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaUmQuadranteComboPorcMaisUnidade")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaTresQuadranteNormalOferta")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaTresQuadranteClubDia")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaTresQuadranteComboLevePague")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaTresQuadranteComboAPartir")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaTresQuadranteComboPorcUmaUnidade")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else if(tipo.equals("etiquetaTresQuadranteComboPorcMaisUnidade")) {
-//			for(int i = 0; i < json.length(); i++) {
-//				System.out.println(json.get(i));
-//			}
-//		}else {
-//			System.out.println("Tipo não encotrado!");
-//		}	
-		}else {
-			
-		}
 	}
-	
 	public void Etiquetar(JSONArray json, String tipo) throws TFormerException {
 		 // Create a new Job instance
 		  Job printJob = new Job();
@@ -80,38 +28,41 @@ public class GeneratorPdf {
 		  int loja = dados.getInt("Loja") ;
 		  // Select the stand-alone FormLayout 
 		  String tff = dados.getString("Tff");
-		  printJob.setRepositoryName ("C:/Users/yagog/Downloads/tff yago/tff yago/PADRAO/"+tff+".tff");
+		  printJob.setRepositoryName ("C:/Users/yagog/Downloads/tff yago/tff yago/PADRAO/"+tff);
 		 // Create a new JobData instance. 
 		 // Here we are using the JobDataRecordSet which accepts Records containing name/value pairs
 		  JobDataRecordSet jobData = new JobDataRecordSet();
-		  
-		 
 		  JobDataRecordSet.Record record;
-		  
-		  for(int i = 0; i < json.length(); i++) {
-			  JSONObject produto = new JSONObject(String.valueOf(json.get(0)));
-			  produto = (JSONObject) produto.get("Etiqueta");
-			  // Create a new Record
-			  record = new JobDataRecordSet.Record();
-			  // Add some name/value pairs for the DataField values to the Record
-			  record.setData("Codigo_de_articulo", produto.getString("Codigo_de_articulo"));
-			  record.setData("Des_Pre_Uni_Med_Fideli", produto.getString("Des_Pre_Uni_Med_Fideli"));
-			  record.setData("Des_Pre_Uni_Med_No_Fideli", produto.getString("Des_Pre_Uni_Med_No_Fideli"));
-			  record.setData("Descripcion_Linea_1", produto.getString("Descripcion_Linea_1"));
-			  record.setData("Descripcion_Linea_2", produto.getString("Descripcion_Linea_2"));
-			  record.setData("Descripcion_Linea_3", produto.getString("Descripcion_Linea_3"));
-			  record.setData("EAN13", produto.getString("EAN13"));
-			  record.setData("Precio_Fidelizado", produto.getString("Precio_Fidelizado"));
-			  record.setData("Precio_NO_Fidelizado", produto.getString("Precio_NO_Fidelizado"));
-			  record.setData("Descuento_Oferta", produto.getString("Descuento_Oferta"));
-			  record.setData("Precio_Neto_Oferta", produto.getString("Precio_Neto_Oferta"));
-			  record.setData("Des_Pre_Uni_Med_Neto_Ofe", produto.getString("Des_Pre_Uni_Med_Neto_Ofe"));
-			  record.setData("Data_Fim_Promo", produto.getString("Data_Fim_Promo"));
-			  
-			  // Add the Record to the JobData
-			  jobData.add(record);
-	
+		 
+		  if(tipo.equals("etiquetaUmQuadranteNormalOferta") || tipo.equals("etiquetaUmQuadranteClubDia")
+			|| tipo.equals("etiquetaTresQuadranteNormalOferta") || tipo.equals("etiquetaTresQuadranteClubDia")) {
+				  
+			  for(int i = 0; i < json.length(); i++) {
+				  JSONObject produto = new JSONObject(String.valueOf(json.get(0)));
+				  produto = (JSONObject) produto.get("Etiqueta");
+				  // Create a new Record
+				  record = new JobDataRecordSet.Record();
+				  // Add some name/value pairs for the DataField values to the Record
+				  record.setData("Codigo_de_articulo", produto.getString("Codigo_de_articulo"));
+				  record.setData("Des_Pre_Uni_Med_Fideli", produto.getString("Des_Pre_Uni_Med_Fideli"));
+				  record.setData("Des_Pre_Uni_Med_No_Fideli", produto.getString("Des_Pre_Uni_Med_No_Fideli"));
+				  record.setData("Descripcion_Linea_1", produto.getString("Descripcion_Linea_1"));
+				  record.setData("Descripcion_Linea_2", produto.getString("Descripcion_Linea_2"));
+				  record.setData("Descripcion_Linea_3", produto.getString("Descripcion_Linea_3"));
+				  record.setData("EAN13", produto.getString("EAN13"));
+				  record.setData("Precio_Fidelizado", produto.getString("Precio_Fidelizado"));
+				  record.setData("Precio_NO_Fidelizado", produto.getString("Precio_NO_Fidelizado"));
+				  record.setData("Descuento_Oferta", produto.getString("Descuento_Oferta"));
+				  record.setData("Precio_Neto_Oferta", produto.getString("Precio_Neto_Oferta"));
+				  record.setData("Des_Pre_Uni_Med_Neto_Ofe", produto.getString("Des_Pre_Uni_Med_Neto_Ofe"));
+				  record.setData("Data_Fim_Promo", produto.getString("Data_Fim_Promo"));
+				  
+				  // Add the Record to the JobData
+				  jobData.add(record);
+		
+			  }
 		  }
+		    
 		  Date data = new Date();
 			Calendar cal = Calendar.getInstance();
 			cal.setTime(data);
